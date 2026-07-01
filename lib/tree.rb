3427264e72e34da0c049070ab4d68f6cbef18629
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-30 19:05:10 -0600

    Addded balance logic if arr.length is odd or even
    modulo is sick but unintuitive, comment added

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,13 +5,25 @@ class Tree
   attr_accessor :tree
 
   def build_tree(arr)
-    root_value = arr[(arr.length / 2).floor]
-    root_node = Node.new(root_value)
-    root_node
+    root_position = (arr.length / 2).floor + ((arr.length % 2) - 1) # odd length: + 0; even length: - 1 (for a left-shift)
+    p " root_pos: #{root_position}" 
+    root_value = arr[root_position]
+    p root_value
+    left_side = arr[0...root_position]
+    p left_side
+    right_side = arr[(root_position + 1)..]
+    p right_side
+    return Node.new(root_value) # if left_side == root_value
+    # maybe a different thing to check?
+
+
+    # root_node = Node.new(root_value, build_tree(left_side), build_tree(right_side))
+    # root_node
   end
 
   def initialize(arr)
     cleaned_arr = arr.sort.uniq
+    p "cleaned arr length: #{cleaned_arr.length}"
     @tree = self.build_tree(cleaned_arr)  
   end
 
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -4,13 +4,19 @@ require_relative '../lib/tree'
 
 
 describe "build_tree" do
-  it "returns the root node (midpoint)" do
-    test = Tree.new([1, 2, 3])
+  it "returns the root node (midpoint: left-shifted) for an even number of elements" do
+    test = Tree.new([1, 2, 3, 4])
     
     expect(test.tree.value).to eq(2)
   end
 
-  it "returns the root node (midpoint) on an unordered array with duplicates" do
+  it "returns the root node (midpoint) for an odd number of elements" do
+    test = Tree.new([1, 2, 3, 4, 5, 6, 7])
+    
+    expect(test.tree.value).to eq(4)
+  end
+
+  xit "returns the root node (midpoint) on an unordered array with duplicates" do
     input = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
     test = Tree.new(input)
     expect(test.tree.value).to eq(8)
