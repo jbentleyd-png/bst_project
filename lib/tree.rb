3049e7ba0172c2ad0d2e78a51c1ab1e378c9dc43
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-07-01 13:19:15 -0600

    Stole a printing method, removed logs.
    Now can visualize the tree without printing all the time.

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,13 +6,9 @@ class Tree
 
   def build_tree(arr)
     root_position = (arr.length / 2).floor + ((arr.length % 2) - 1) # odd length: + 0; even length: - 1 (for a left-shift)
-    p " root_pos: #{root_position}" 
     root_value = arr[root_position]
-    p root_value
     left_side = arr[0...root_position]
-    p left_side
     right_side = arr[(root_position + 1)..]
-    p right_side
     return Node.new(root_value) if right_side == []
 
 
@@ -22,8 +18,15 @@ class Tree
 
   def initialize(arr)
     cleaned_arr = arr.sort.uniq
-    p "cleaned arr length: #{cleaned_arr.length}"
     @tree = self.build_tree(cleaned_arr)  
+  end
+
+  def pretty_print(node = @tree, prefix = '', is_left: true)
+    return unless node
+
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false)
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true)
   end
 
 
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -4,32 +4,32 @@ require_relative '../lib/tree'
 
 
 describe "build_tree" do
-  xit "returns the root node (midpoint: left-shifted) for an even number of elements" do
+  it "returns the root node (midpoint: left-shifted) for an even number of elements" do
     test = Tree.new([1, 2, 3, 4])
     
     expect(test.tree.value).to eq(2)
   end
 
-  xit "returns the root node (midpoint) for an odd number of elements" do
+  it "returns the root node (midpoint) for an odd number of elements" do
     test = Tree.new([1, 2, 3, 4, 5, 6, 7])
     
     expect(test.tree.value).to eq(4)
   end
 
 
-  xit "returns the root node (midpoint) on an unordered array with duplicates" do
+  it "returns the root node (midpoint) on an unordered array with duplicates" do
     input = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
     test = Tree.new(input)
     expect(test.tree.value).to eq(8)
   end
 
-  xit "base case test 1" do
+  it "base case test 1" do
     test = Tree.new([1])
     
     expect(test.tree.value).to eq(1)
   end
 
-  xit "base case test 2" do
+  it "base case test 2" do
     test = Tree.new([1, 2])
     
     expect(test.tree.value).to eq(1)
@@ -41,6 +41,16 @@ describe "build_tree" do
     return unless test.tree.left.value == 1
     return unless test.tree.right.value == 3
     three_node = test.tree.right
+    test.pretty_print
     expect(three_node.right.value).to eq(4)
+  end
+end
+
+
+describe "pretty_print" do
+  it "seems to work" do
+    input = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+    test = Tree.new(input)
+    expect(test.pretty_print).to eq(nil)
   end
 end
